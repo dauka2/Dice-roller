39154fa9368c1738258b044895951e69eeab9d56
dauka2 <93971839+dauka2@users.noreply.github.com>
2022-12-26 19:13:30 +0600

    Add files via upload

--- diceroller/MainActivity.kt
+++ diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -19,9 +19,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val dice = Dice(6)//makes number of dices that will be rolled
-        val diceRoll = dice.roll()//rolling
-        val resultTextView: TextView = findViewById(R.id.textView)// Update the screen with the dice roll
-        resultTextView.text = diceRoll.toString()
+        // Create new Dice object with 6 sides and roll the dice
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView2)
+
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
